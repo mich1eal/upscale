4965b9c5dc66fcbe2b5531cf8223c6a8e01a2af8
mich1eal <msmille3@gmail.com>
2020-04-12 21:14:22 -0400

    First commit, default android studio files

---
+++ app/src/main/java/com/mich1eal/upscale/Scale.java
@@ -0,0 +1,13 @@
+package com.mich1eal.upscale;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Scale extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scale);
+    }
+}
